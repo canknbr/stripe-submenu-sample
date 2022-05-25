9207c2e736f6c27ca04fb8db7034bd3ff0533947
Can Kanbur <cankanbur1@gmail.com>
2022-05-25 16:24:54 +0300

    .

--- src/context.js
+++ src/context.js
@@ -1,17 +1,48 @@
 import React, { useState, useContext } from 'react';
 import sublinks from './data';
+const AppContext = React.createContext();
 
-const SubmenuContext = React.createContext();
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find(link => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
-const SubmenuProvider = ({ children }) => {
   return (
-    <SubmenuContext.Provider value={'hello'}>
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
       {children}
-    </SubmenuContext.Provider>
+    </AppContext.Provider>
   );
 };
-
+// make sure use
 export const useGlobalContext = () => {
-  return useContext(SubmenuContext);
+  return useContext(AppContext);
 };
-export default SubmenuProvider;
+
+export { AppContext, AppProvider };
